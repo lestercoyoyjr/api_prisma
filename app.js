2a33ce800db1da7847ae9d5e21ccd54ce4e0eeb8
lestercoyoy <lester_cch@hotmail.com>
2022-08-24 11:34:18 -0600

    ADD
    Post method

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/', (req,res) => {
 })
 
 // ENDPOINTS
+app.post('/post', async(req,res)=>{
+    const {title,content} = req.body
+    const result = await prisma.post.create({
+        data:{
+            title,content
+        }
+    })
+    res.json(result)
+})
 
 app.listen(3001, () =>
     console.log(`Server ready at: http//:localhost:3001`)
